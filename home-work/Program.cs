a90716381092cd9a2868cf020461c17c95d5c206
Aleksandr Fomichev <aleksandrfomicev260477@gmail.com>
2024-06-03 22:20:54 +0300

    Написал решение.

---
+++ home-work/Program.cs
@@ -0,0 +1,91 @@
+﻿/*
+Задача 1.
+*/
+
+Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNaturalNumbers(int m, int n)
+
+{
+    if (m == 0 || n == 0) return;
+
+    if (m < n)
+    {
+        Console.Write($"{m}, ");
+        PrintNaturalNumbers(m + 1, n);
+        return;
+    }
+
+    else
+    {
+        if (m > n)
+        {
+            Console.Write($"{m}, ");
+            PrintNaturalNumbers(m - 1, n);
+            return;
+        }
+    }
+
+    if (m == n)
+    {
+        Console.Write($"{m}, ");
+        return;
+    }
+}
+
+PrintNaturalNumbers(m, n);
+
+
+
+
+/*
+Задача 2.
+*/
+
+Console.WriteLine("Введите натуральное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FunctionAkk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return FunctionAkk(m - 1, 1);
+    else return FunctionAkk(m - 1, FunctionAkk(m, n - 1));
+}
+
+Console.Write($"A(m,n) = {FunctionAkk(m, n)}");
+
+
+
+
+
+/*
+Задача 3.
+*/
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+
+string PrintArray(int[] array)
+{
+    return string.Join(", ", array);
+}
+int[] ArraySwap(int[] array, int index)
+{
+
+    if (index <= 0)
+    {
+        return array;
+    }
+    int temp = array[index];
+    array[index] = array[array.Length - index - 1];
+    array[array.Length - index - 1] = temp;
+    return ArraySwap(array, index -= 2);
+}
+
+Console.WriteLine($"{PrintArray(ArraySwap(array, array.Length - 1))}");
